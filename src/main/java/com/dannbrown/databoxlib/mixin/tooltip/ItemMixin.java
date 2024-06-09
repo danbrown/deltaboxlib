d727104b2d5d71ef300132345a2e868ef88ef1e1
Daniel Brown <danielbrown25.9.2@gmail.com>
2024-06-09 16:43:19 -0300

    optimized item tooltip mixin

--- src/main/java/com/dannbrown/databoxlib/mixin/tooltip/ItemMixin.java
+++ src/main/java/com/dannbrown/databoxlib/mixin/tooltip/ItemMixin.java
@@ -3,14 +3,12 @@ package com.dannbrown.databoxlib.mixin.tooltip;
 
 import com.dannbrown.databoxlib.lib.LibLang;
 import net.minecraft.ChatFormatting;
-import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.level.Level;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -19,15 +17,10 @@ import java.util.List;
 
 @Mixin(Item.class)
 public abstract class ItemMixin {
-  @Shadow
-  protected abstract String getOrCreateDescriptionId();
-
-  @Shadow public abstract void verifyTagAfterLoad(CompoundTag pTag);
-
   @Inject(method = { "appendHoverText" }, at = { @At("HEAD") }, require = 1)
   public void appendHoverText(ItemStack itemStack, Level level, List<Component> components, TooltipFlag tooltipFlag, CallbackInfo ci) {
     // create or get an item description id, get the last key and add as suffix to
-    String itemDescription = this.getOrCreateDescriptionId();
+    String itemDescription = itemStack.getDescriptionId();
     String[] parts = itemDescription.split("\\.");
     String tooltipTranslationKey = parts.length > 1 ? LibLang.Companion.getTooltipKey(parts[parts.length - 2], parts[parts.length - 1]) : "";
     String genericTranslationKey = parts.length > 1 ? LibLang.Companion.getTooltipKey(null, parts[parts.length - 1]) : "";
