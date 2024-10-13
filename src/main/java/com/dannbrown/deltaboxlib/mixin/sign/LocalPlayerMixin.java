696ee3380cc15178e290bf83fd1ad6ee75d5313e
Daniel Brown <danielbrown25.9.2@gmail.com>
2024-10-12 23:53:02 -0300

    hanging sign gui render

---
+++ src/main/java/com/dannbrown/deltaboxlib/mixin/sign/LocalPlayerMixin.java
@@ -0,0 +1,27 @@
+package com.dannbrown.deltaboxlib.mixin.sign;
+
+import com.dannbrown.deltaboxlib.content.blockEntity.GenericHangingSignBlockEntity;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.screens.inventory.HangingSignEditScreen;
+import net.minecraft.client.player.LocalPlayer;
+import net.minecraft.world.level.block.entity.SignBlockEntity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin( LocalPlayer.class )
+public class LocalPlayerMixin {
+
+  @Shadow
+  protected final Minecraft minecraft = Minecraft.getInstance();
+
+  @Inject(at = @At( "HEAD" ), method = "openTextEdit")
+  private void openTextEdit(SignBlockEntity pSignEntity, boolean pIsFrontText, CallbackInfo ci) {
+    if (pSignEntity instanceof GenericHangingSignBlockEntity hangingsignblockentity) {
+      this.minecraft.setScreen(new HangingSignEditScreen(hangingsignblockentity, pIsFrontText, this.minecraft.isTextFilteringEnabled()));
+      ci.cancel();
+    }
+  }
+}
