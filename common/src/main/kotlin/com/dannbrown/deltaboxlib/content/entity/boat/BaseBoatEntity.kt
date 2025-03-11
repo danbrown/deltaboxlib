a4c9c11c7e9ff825884b1ee3c6d2b0436bc2d058
Daniel Brown <danielbrown25.9.2@gmail.com>
2025-03-10 23:13:58 -0300

    boat entities

---
+++ common/src/main/kotlin/com/dannbrown/deltaboxlib/content/entity/boat/BaseBoatEntity.kt
@@ -0,0 +1,70 @@
+package com.dannbrown.deltaboxlib.content.entity.boat
+
+import net.minecraft.nbt.CompoundTag
+import net.minecraft.network.syncher.EntityDataAccessor
+import net.minecraft.network.syncher.EntityDataSerializers
+import net.minecraft.network.syncher.SynchedEntityData
+import net.minecraft.world.entity.EntityType
+import net.minecraft.world.entity.vehicle.Boat
+import net.minecraft.world.item.Item
+import net.minecraft.world.level.Level
+import java.util.function.Supplier
+
+class BaseBoatEntity(
+  private val boatItem: Supplier<Item>,
+  val variant: String,
+  private val pEntityType: Supplier<EntityType<out Boat>>,
+  pLevel: Level
+) : Boat(pEntityType.get(), pLevel) {
+  constructor(
+    boatItem: Supplier<Item>,
+    variant: String,
+    pEntityType: Supplier<EntityType<out Boat>>,
+    level: Level,
+    pX: Double,
+    pY: Double,
+    pZ: Double
+  ) : this(boatItem, variant, pEntityType, level) {
+    this.setPos(pX, pY, pZ)
+    this.xo = pX
+    this.yo = pY
+    this.zo = pZ
+    setVariant(variant)
+  }
+
+  init {
+    setVariant(variant)
+  }
+
+  override fun getDropItem(): Item {
+    return boatItem.get()
+  }
+
+  fun setVariant(name: String) {
+    try {
+      entityData.set(DATA_ID_TYPE, name)
+    } catch (err: IllegalArgumentException) {
+      throw IllegalArgumentException("Invalid boat variant: $name")
+    }
+  }
+
+  override fun defineSynchedData() {
+    super.defineSynchedData()
+    entityData.define(DATA_ID_TYPE, "oak") // Default variant
+  }
+
+  override fun addAdditionalSaveData(pCompound: CompoundTag) {
+    pCompound.putString("Type", this.entityData.get(DATA_ID_TYPE))
+  }
+
+  override fun readAdditionalSaveData(pCompound: CompoundTag) {
+    if (pCompound.contains("Type", 8)) {
+      this.setVariant(pCompound.getString("Type"))
+    }
+  }
+
+  companion object {
+    val DATA_ID_TYPE: EntityDataAccessor<String> =
+      SynchedEntityData.defineId(Boat::class.java, EntityDataSerializers.STRING)
+  }
+}
