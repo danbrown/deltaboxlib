84110a951b15bb44f2c9da2321ec6798a2671cc8
Daniel Brown <danielbrown25.9.2@gmail.com>
2025-03-22 19:44:57 -0300

    fixed item entry types

--- common/src/main/kotlin/com/dannbrown/deltaboxlib/registrate/registry/ItemEntry.kt
+++ common/src/main/kotlin/com/dannbrown/deltaboxlib/registrate/registry/ItemEntry.kt
@@ -5,18 +5,18 @@ import net.minecraft.world.item.Item
 import java.util.function.Supplier
 
 data class ItemEntry<T : Item>(private val builder: ItemBuilder<T>?) {
-  constructor(item: Supplier<Item>) : this(null) {
+  constructor(item: Supplier<T>) : this(null) {
     setupItem = item
   }
 
-  private var setupItem: Supplier<Item>? = null
+  private var setupItem: Supplier<T>? = null
 
   fun getBuilder(): ItemBuilder<T> {
     if (builder == null) throw NoSuchFieldError("Cannot get the builder from an external block entry.")
     return builder
   }
 
-  fun get(): Item {
+  fun get(): T {
     return if (setupItem !== null) setupItem!!.get() else if (builder !== null) builder.getItem()
       .get() else throw throw NoSuchFieldError("This block entry is invalid")
   }
